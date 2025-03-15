2e9baaa3f0a7670336cd6c0ce188499913baca7d
Denis Zakharchuk <albertgutenberg1@gmail.com>
2025-03-15 06:00:02 +0300

    {fix}: deleted unused test

--- src/test/java/com/example/demo/KafkaProducerServiceTests.java
+++ src/test/java/com/example/demo/KafkaProducerServiceTests.java
@@ -8,7 +8,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.core.KafkaTemplate;
 
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
